c98b3d6ba2322a893e1a5019e9a486f2ad27e584
yolanda holgado martin <yolanda.holgado@hotmail.com>
2021-10-01 17:13:55 +0200

    iteration1

---
+++ iteration1.js
@@ -0,0 +1,16 @@
+
+let myFavoriteHero="Hulk";
+console.log(myFavoriteHero);
+
+let x =50;
+console.log(x);
+
+let h=5;
+console.log(h);
+
+let y=10;
+console.log(y);
+
+let z =h+y;
+console.log(z);
+
