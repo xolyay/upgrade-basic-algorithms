3a17ebb4b31eb9aba1964328f62cca2f42590b25
yolanda holgado martin <yolanda.holgado@hotmail.com>
2021-10-05 15:19:43 +0200

    interacion 3 y 4

---
+++ app.js
@@ -0,0 +1,136 @@
+
+let myFavoriteHero="Hulk";
+console.log(myFavoriteHero);
+
+let x =50;
+console.log(x);
+
+let h=5;
+console.log(h);
+
+let y=10;
+console.log(y);
+
+let z =h+y;
+console.log(z);
+
+/*ITERATION 2*/
+
+
+
+
+
+1.1
+const character= {name:'Jack Sparrow', age: 25};
+
+console.log(character);
+
+1.2
+
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24; 
+let comment= 'me gustan los lobos.';
+
+let message=  'Soy' + ' ' + firstName + ' ' + lastName + ' tengo ' + age + ' años' + ' ' + 'y' + ' ' + comment; 
+console.log (message);
+
+1.3 
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+let finalPrice = toy1.price + toy2.price 
+
+console.log (finalPrice)
+
+
+1.4
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+
+
+
+
+/*Operadores*/
+
+1.1
+
+let suma = (10 * 5)
+console.log(suma);
+
+1.2
+
+let divi = 10 / 5
+
+console.log (divi);
+
+1.3
+let numS =15
+let numP = 9
+
+console.log(numS/numP);
+1.4
+
+let y= 10
+let z= 5
+console.log (y+z)
+
+
+1.5
+
+console.log(y*z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1.1
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0])
+1.2
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+avengerss[0] = "IRONMAN"
+
+1.3
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers.length)
+1.4
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+rickAndMortyCharacters.push()
+
+console.log(rickAndMortyCharacters[2])
+1.5
+const rickAndMortyCharacterss = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacterss.pop()
+
+console.log(rickAndMortyCharacterss)
+1.6
+const rickAndMortyCharactersss = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharactersss.splice(1, 1);
+
+console.log(rickAndMortyCharactersss)
+
+console.log (x);
+
+
+
+
+
