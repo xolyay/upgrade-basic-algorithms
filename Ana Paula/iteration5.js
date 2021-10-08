51c048e0b62180a7333d3f83c6dafb57d415d8c9
yolanda holgado martin <yolanda.holgado@hotmail.com>
2021-10-08 15:39:48 +0200

    elimino archivos innecesarios

--- Ana Paula/iteration5.js
+++
@@ -1,4 +0,0 @@
-function rollDice(numCaras) {
-    return Math.floor(Math.random() * (numCaras - 1)) + 1;
-}
-console.log(rollDice(6));
--- Ana Paula/iteration6.js
+++
@@ -1,9 +0,0 @@
-['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño']
-
- function swap(array,i,j){
-    let temp=array[i];
-    array[i]=array[j];
-    array[j]=temp;
-    console.log(array);
- }
- swap(['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño'],0,2);
